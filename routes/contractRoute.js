9f9161cd73a1ab09f2b55dea2b75ab0dbf03d3de
tatendabakozw <tatendabakozw@gmail.com>
2021-10-11 02:02:01 +0200

    fix: create contract other user id

--- routes/contractRoute.js
+++ routes/contractRoute.js
@@ -3,17 +3,19 @@ const { requireSignIn } = require('../middleware')
 const Contract = require('../models/Contract')
 const User = require('../models/User')
 const router = express.Router()
+const Service = require('../models/Service')
 
 router.post('/create/:id', requireSignIn, async (req, res, next) => {
     const { id } = req.params  //id of the user to receive
     const _user = req.user // the sender of the contract
     try {
         const { firstname, lastname, title, company, email, phone_number, details, payment_type, payment_period, amount, period_of_contract } = req.body
+        const receiver = await Service.findOne({ _id: id })
 
         //contract
         const new_contract = new Contract({
             createdBy: _user._id,
-            sent_to: id,
+            sent_to: receiver.owner,
             firstname,
             lastname,
             title,
@@ -29,7 +31,7 @@ router.post('/create/:id', requireSignIn, async (req, res, next) => {
 
         // save contrat and push to user documents
         new_contract.save().then(response => {
-            User.findOneAndUpdate({ _id: id }, { $push: { contracts: response._id } }).then(() => {
+            User.findOneAndUpdate({ _id: receiver.owner }, { $push: { contracts: response._id } }).then(() => {
                 User.findOneAndUpdate({ _id: _user._id }, { $push: { contracts: response._id } }).then(() => {
                     res.status(200).json({ response })
                 })
@@ -43,8 +45,8 @@ router.post('/create/:id', requireSignIn, async (req, res, next) => {
     }
 })
 
-//gert all user routesx
-router.get('/user/:id',requireSignIn, async (req,res,next) =>{
+//gert all user contracts
+router.get('/user/:id', requireSignIn, async (req, res, next) => {
 
 })
 
